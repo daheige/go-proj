04facb0776447d40c2c35e7b1e72f95ee857ca02
daheige <zhuwei313@hotmail.com>
2020-08-09 16:28:45 +0800

    generate code shell os

--- bin/php-generate.sh
+++ bin/php-generate.sh
@@ -47,14 +47,15 @@ mv $root_dir/clients/php/GPBMetadata $php_client_dir/App/Grpc/
 #为了避免composer install grpc protobuf的命名空间GPBMetadata冲突
 #这里需要把GPBMetadata加上命名空间
 #在GPBMetadata前面加上命名空间App\Grpc
-#linux系统请打开这个注释
-#sed -i 's/GPBMetadata/App\\\Grpc\\\GPBMetadata/g' `grep GPBMetadata -rl $php_client_dir/App/Grpc/`
-#sed -i 's/\\App\\Grpc\\GPBMetadata\\Google/\/\/ \\App\Grpc\\\GPBMetadata\\\Google/g' `grep GPBMetadata -rl $php_client_dir/App/Grpc/`
-
-# mac unix系统
-sed -i "" 's/GPBMetadata/App\\\Grpc\\\GPBMetadata/g' `grep GPBMetadata -rl $php_client_dir/App/Grpc/`
-
-sed -i "" 's/\\App\\Grpc\\GPBMetadata\\Google/\/\/ \\App\Grpc\\\GPBMetadata\\\Google/g' `grep GPBMetadata -rl $php_client_dir/App/Grpc/`
+os=`uname -s`
+if [ $os == "Darwin" ];then
+    # mac unix系统
+    sed -i "" 's/GPBMetadata/App\\\Grpc\\\GPBMetadata/g' `grep GPBMetadata -rl $php_client_dir/App/Grpc/`
+    sed -i "" 's/\\App\\Grpc\\GPBMetadata\\Google/\/\/ \\App\Grpc\\\GPBMetadata\\\Google/g' `grep GPBMetadata -rl $php_client_dir/App/Grpc/`
+else
+    sed -i 's/GPBMetadata/App\\\Grpc\\\GPBMetadata/g' `grep GPBMetadata -rl $php_client_dir/App/Grpc/`
+    sed -i 's/\\App\\Grpc\\GPBMetadata\\Google/\/\/ \\App\Grpc\\\GPBMetadata\\\Google/g' `grep GPBMetadata -rl $php_client_dir/App/Grpc/`
+fi
 
 echo "\n\033[0;32mGenerate codes successfully!\033[39;49;0m\n"
 
